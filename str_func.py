6c7ef9077c9cf5cf3a568002ea8fe4193a452392
Vadim Andreev <vadimcola@mail.ru>
2023-02-13 23:41:55 +0700

    add  first_letter func

--- str_func.py
+++ str_func.py
@@ -1,6 +1,20 @@
 
-
 def register(word):
+    """
+    возврат текста в верхнем регистре
+    :param word:
+    :return:
+    """
     return word.upper()
+
+
+def first_letter(word):
+    """
+    возврат текста в первой заглавной буквой
+    :param word:
+    :return:
+    """
+    return word.capitalize()
+
 
 
